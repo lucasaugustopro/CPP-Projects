da15ca9807c05246ec5d1a40aa8876ee10e61a73
Lucas Augusto <lucasaugustopro77@gmail.com>
2023-03-30 18:24:02 -0300

    add testing file

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string teste;
+    teste = "Apenas um teste";
+
+    for(int i = 0; i <= 50; i++){
+        cout << teste << endl;
+    }
+
+    return 0;
+}
